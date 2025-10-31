07e6afd6924cf157fd2e355a5278bcac0e7da0b6
Your Name <you@example.com>
2025-10-31 21:44:31 +0500

    kdjakfj

--- library/admin.py
+++ library/admin.py
@@ -1,23 +1,24 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 from .models import Book, CartItem, Favorite, UserProfile
 
 @admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
+class BookAdmin(ModelAdmin):
     list_display = ['title', 'author', 'genre', 'price', 'rating']
     list_filter = ['genre', 'created_at']
     search_fields = ['title', 'author']
 
 @admin.register(CartItem)
-class CartItemAdmin(admin.ModelAdmin):
+class CartItemAdmin(ModelAdmin):
     list_display = ['user', 'book', 'quantity', 'added_at']
     list_filter = ['added_at']
 
 @admin.register(Favorite)
-class FavoriteAdmin(admin.ModelAdmin):
+class FavoriteAdmin(ModelAdmin):
     list_display = ['user', 'book', 'added_at']
     list_filter = ['added_at']
 
 @admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
+class UserProfileAdmin(ModelAdmin):
     list_display = ['user', 'city', 'country', 'created_at']
     list_filter = ['created_at']
